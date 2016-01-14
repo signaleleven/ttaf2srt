e42479c553b0d92eaa7afe6268117398c26b104c
Michael Sacchi <michael.sacchi@signal-eleven.com>
2016-01-14 10:57:17 +0100

    Radical changes to make it work on ZDF mediathek subs and produce a SRT that works on my Samsung TV.

--- ttaf2srt.py
+++ ttaf2srt.py
@@ -19,19 +19,24 @@ Note - only tested on German 'Tatort' subtitles.
 Note2 - if using vlc or mplayer, make sure to specify 'utf8' as encoding, otherwise, special characters will not render correctly.
 """
 import sys
+import datetime
 from xml.dom import minidom
+
+def strfdelta(tdelta, fmt):
+    d = {"days": tdelta.days}
+    d["hours"], rem = divmod(tdelta.seconds, 3600)
+    d["minutes"], d["seconds"] = divmod(rem, 60)
+    return fmt.format(**d)
 
 def dumpText(item):
     for child in item.childNodes:
         if child.nodeType == child.TEXT_NODE:
             print(child.nodeValue, end="")
         elif child.nodeType == child.ELEMENT_NODE:
-            if child.nodeName == "tt:br":
+            if child.nodeName == "br":
                 print()
-            elif child.nodeName == "tt:span":
-                print("<font color=\"" + styles[child.getAttribute("style")] + "\">", end="")
+            elif child.nodeName == "span":
                 dumpText(child)
-                print("</font>", end="")
             else:
                 print("Unknown Node: " + child.nodeName, file=sys.stderr)
 
@@ -41,40 +46,40 @@ def dumpHeader(item, subCount):
     end = item.getAttribute("end")
     # ### this is a silly hack - for some reason, my ttaf files all start at hour 10? Resetting
     # the hour makes it work again
-    begin = '0' + begin[1:]
-    end = '0' + end[1:]
-    print(begin + " --> " + end)
+    #begin = '0' + begin[1:]
+    #end = '0' + end[1:]
+    tbegin=datetime.timedelta(seconds=float(begin))
+    tend=datetime.timedelta(seconds=float(end))
+    subsecb=int(str(float(begin) % 1)[2:5])
+    subsece=int(str(float(end) % 1)[2:5])
+    print("%.2d:%.2d:%.2d,%.3d --> %.2d:%.2d:%.2d,%.3d" % (tbegin.seconds//3600,(tbegin.seconds//60)%60,tbegin.seconds%60,subsecb,tend.seconds//3600,(tend.seconds//60)%60,tend.seconds%60,subsece))
+    #print(begin + " --> "+ end)
 
 def parseStyles(styles):
     result = {}
     for style in styles:
-        result[style.getAttribute('xml:id')] = style.getAttribute('tts:color')
+        result[style.getAttribute('xml:id')] = style.getAttribute('color')
     return result
 
 with open(sys.argv[1]) as f:
     xmldoc = f.read().replace('\n', ' ').replace('\r', '')
 xmldoc = minidom.parseString(xmldoc)
 
-header = xmldoc.getElementsByTagName('tt:head')
+header = xmldoc.getElementsByTagName('head')
 if len(header):
-    styling = header[0].getElementsByTagName('tt:styling')
+    styling = header[0].getElementsByTagName('styling')
     if len(styling):
-        styles = parseStyles(styling[0].getElementsByTagName('tt:style'))
+        styles = parseStyles(styling[0].getElementsByTagName('style'))
 
-body = xmldoc.getElementsByTagName('tt:body')
+body = xmldoc.getElementsByTagName('body')
 
-itemlist = body[0].getElementsByTagName('tt:p') 
+itemlist = body[0].getElementsByTagName('p') 
 
-subCount = 0
+subCount = 1
 
 for item in itemlist:
-    if item.hasAttribute('xml:id'):
         dumpHeader(item, subCount)
         subCount += 1
-        color = styles[item.getAttribute("style")]
-        if color:
-            print("<font color=\"" + color + "\">", end="")
+        color = item.getAttribute("tts:style")
         dumpText(item)
-        if color:
-            print("</font>", end="")
         print("\n")
